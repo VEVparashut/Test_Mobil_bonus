74bd4ef290b2ee9ea177865d8146e21463333558
Neto test <89373786148@yandex.ru>
2023-01-04 20:55:29 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int refill = 1100;
+        int bonus;
+
+        if (refill >=1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance_new = balance + refill + bonus;
+
+        System.out.println("Бонус = " + bonus);
+        System.out.println("Итоговый баланс = " + balance_new + " рублей");
+    }
+}
